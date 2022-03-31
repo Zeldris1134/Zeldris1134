b5cf272a3838bb19ed051f0208802999afbf4029
Zeldris1134 <88009815+Zeldris1134@users.noreply.github.com>
2022-03-31 16:07:42 +1300

    Add files via upload

---
+++ calculator/js/script.js
@@ -0,0 +1,57 @@
+const number1 = document.getElementById("fnumber");
+const number2 = document.getElementById("snumber");
+const answer = document.getElementById("answer");
+const additionBtn = document.querySelector(".plus");
+const subtractionBtn = document.querySelector(".minus");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
+const modusBtn = document.querySelector(".modus");
+const clearBtn = document.querySelector(".clear");
+const closeBtn = document.querySelector(".close");
+
+const addition = function () {
+  let sum = ~~number1.value + ~~number2.value;
+  document.getElementById("answer").innerText = sum;
+};
+
+const subtraction = function () {
+  let sum = ~~number1.value - ~~number2.value;
+  document.getElementById("answer").innerText = sum;
+};
+
+const multiply = function () {
+  let sum = ~~number1.value * ~~number2.value;
+  document.getElementById("answer").innerText = sum;
+};
+
+const divide = function () {
+  let sum = ~~number1.value / ~~number2.value;
+  document.getElementById("answer").innerText = sum;
+};
+
+const remainder = function () {
+  let sum = ~~number1.value % ~~number2.value;
+  document.getElementById("answer").innerText = sum;
+};
+
+const clear = function () {
+  document.getElementById("fnumber").value = 0;
+  document.getElementById("snumber").value = 0;
+  document.getElementById("answer").innerText = 0;
+};
+
+const close = function () {
+  document.querySelector(".close").classList.add("hidden");
+  document.querySelector(".calc").classList.add("hidden");
+  document.querySelector(".button-wrap").classList.add("hidden");
+  document.querySelector(".answer-container").classList.add("hidden");
+  document.querySelector(".closed").classList.remove("hidden");
+};
+
+additionBtn.addEventListener("click", addition);
+subtractionBtn.addEventListener("click", subtraction);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+modusBtn.addEventListener("click", remainder);
+clearBtn.addEventListener("click", clear);
+closeBtn.addEventListener("click", close);
