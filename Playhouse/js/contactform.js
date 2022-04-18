46f91bf47d999f16b1b61d71a339daa5a8eff064
Zeldris1134 <88009815+Zeldris1134@users.noreply.github.com>
2022-04-19 10:08:14 +1200

    Update contactform.js

--- Playhouse/js/contactform.js
+++ Playhouse/js/contactform.js
@@ -6,8 +6,6 @@ const contactFirstName = document.querySelector("#contact-FName");
 const contactLastName = document.querySelector("#contact-LName");
 const contactMobileNumber = document.querySelector("#contact-mobile");
 const contactMessage = document.querySelector("#contact-message");
-const validRegex =
-  /^[a-zA-Z0-9.!#$%&'*+/=?^_'{|}~-]+@[[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 submitBtn.addEventListener("click", (e) => {
   if (contactEmail.value.match(validRegex)) {
